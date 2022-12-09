67528e718fa7a5c3a9fcb68fc0bdee5d4ee99918
Haruki <m.haruki20020226@gmail.com>
2022-12-09 13:33:32 +0900

    javascript一部実装

---
+++ index.js
@@ -0,0 +1,12 @@
+const main = document.querySelectorAll(".menu");
+const item = Array.prototype.slice.call(main,0);
+item.forEach(function (element) {
+    element.addEventListener("mouseover",function() {
+        element.querySelector(".sub-menu").classList.add("open");
+        element.querySelector(".sub-menu").style.display="block";
+    },false);
+    element.addEventListener("mouseout",function() {
+        element.querySelector(".sub-menu").classList.remove("open");
+        element.querySelector(".sub-menu").style.display="none";
+    },false);
+});
