31431df519f897bc9e728ed42463b3391e4b76bf
Haruki <m.haruki20020226@gmail.com>
2022-12-15 15:06:29 +0900

    jQueryに変更中

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const main = document.querySelectorAll(".menu");
-const item = Array.prototype.slice.call(main,0);
-item.forEach(function (element) {
-    element.addEventListener("mouseover",function() {
-        element.querySelector(".sub-menu").classList.add("open");
-        element.querySelector(".sub-menu").style.display="block";
-    },false);
-    element.addEventListener("mouseout",function() {
-        element.querySelector(".sub-menu").classList.remove("open");
-        element.querySelector(".sub-menu").style.display="none";
-    },false);
-});
+// const main = document.querySelectorAll(".menu");
+// const item = Array.prototype.slice.call(main,0);
+// item.forEach(function (element) {
+//     element.addEventListener("mouseover",function() {
+//         element.querySelector(".sub-menu").classList.add("open");
+//         element.querySelector(".sub-menu").style.display="block";
+//     },false);
+//     element.addEventListener("mouseout",function() {
+//         element.querySelector(".sub-menu").classList.remove("open");
+//         element.querySelector(".sub-menu").style.display="none";
+//     },false);
+// });
+
+// Javascriptのコード
+
+$(function() {
+    $(".menu-a").hover(
+        function() {
+            $(this).next().slideDown();
+        },
+        function() {
+            $(this).next().slideUp();
+        }
+    );
+})
+
 
 const Globalnav = document.querySelector(".global-nav");
 const navbtn = document.querySelector(".nav-btn");
